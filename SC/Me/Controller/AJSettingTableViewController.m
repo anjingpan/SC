370cbba9dc0c70635994a31424af50bc15868d97
潘安静 <172244892@qq.com>
2017-03-07 23:36:04 +0800

    设置页面添加弹出框

--- SC/Me/Controller/AJSettingTableViewController.m
+++ SC/Me/Controller/AJSettingTableViewController.m
@@ -8,6 +8,7 @@
 
 #import "AJSettingTableViewController.h"
 
+#define kCacheIndexpath [NSIndexPath indexPathForRow:1 inSection:0]
 @interface AJSettingTableViewController ()
 @property (weak, nonatomic) IBOutlet UILabel *cacheLabel;
 
@@ -29,10 +30,28 @@
 #pragma mark - UITableView Delegate
 - (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath{
     [tableView deselectRowAtIndexPath:indexPath animated:YES];
+    if ([indexPath isEqual:kCacheIndexpath]) {
+        UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"" message:@"确认清理" preferredStyle:UIAlertControllerStyleAlert];
+        UIAlertAction *confirm = [UIAlertAction actionWithTitle:@"确认" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+            
+        }];
+        UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+        [alert addAction:confirm];
+        [alert addAction:cancel];
+        [self presentViewController:alert animated:YES completion:nil];
+    }
 }
 
 #pragma mark - logout
 - (IBAction)logout:(id)sender {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"" message:@"退出后将无法完全体验应用" preferredStyle:UIAlertControllerStyleActionSheet];
+    UIAlertAction *logout = [UIAlertAction actionWithTitle:@"确定退出" style:UIAlertActionStyleDestructive handler:^(UIAlertAction * _Nonnull action) {
+        
+    }];
+    UIAlertAction *cancel = [UIAlertAction actionWithTitle:@"取消" style:UIAlertActionStyleCancel handler:nil];
+    [alert addAction:logout];
+    [alert addAction:cancel];
+    [self presentViewController:alert animated:YES completion:nil];
 }
 
 
